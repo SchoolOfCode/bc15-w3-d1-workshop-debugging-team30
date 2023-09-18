61d05f2a60c38727eded298c3dbe1976b2a2106f
Quinnlizzy <quin27@hotmail.co.uk>
2023-09-18 14:48:35 +0100

    ticket 2 complete

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,13 +1,16 @@
-Functin extractInitialsFromFullName(fullName) {
+function extractInitialsFromFullName(fullName) {
 /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
-const         names            = fullName.split(" ");
+const names = fullName.split(" ");
 let initials = "";
 
-For (let i = 0; i < names.length; i++) [
-Const currentName = names[i];
+for (let i = 0; i < names.length; i++) {
+const currentName = names[i];
 const currentInitial = currentName.at(0);
 initials += currentInitial;
 }
 
-Return initials;
-)
+return initials;
+}
+extractInitialsFromFullName('Siyu Duan');
+
+console.log (extractInitialsFromFullName('Siyu Duan'));
