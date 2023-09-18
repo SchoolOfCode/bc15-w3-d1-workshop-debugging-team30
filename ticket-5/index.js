62c06a860c05d278f72ad02e891a76cf4305e68c
Quinnlizzy <quin27@hotmail.co.uk>
2023-09-18 16:12:13 +0100

    ticket5problemsolving

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -1,27 +1,29 @@
-const shoppingCart = [
-  {
+console.log('hello world');
+
+const shoppingCart = {
+  item1: {
     product: "Bananas",
     price: 0.5,
     quantity: 6,
   },
-  {
+  item2: {
     product: "Apples",
-
     price: 0.7,
     quantity: 8,
   },
-  {
+  item3: {
     product: "Yoghurt",
     price: 1.5,
     quantity: 10,
   },
-];
+};
+
 
 let totalCost = 0;
 
 for (let i = 0; i <= shoppingCart.length; i++) {
   const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
+  let costForItem = shoppingCart[i].price * shoppingCart[i].quantity;
   totalCost += costForItem;
 }
 
